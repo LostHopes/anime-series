ff6877593dd5aeb724b6da9cd458b43b4d38a839
losthope <arsen.melnuchyk@gmail.com>
2024-12-19 16:54:48 +0200

    tests: for API

---
+++ backend/src/tests/test_anime.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+from anime import app
+
+
+client = TestClient(app)
+
+
+def test_get_all_anime():
+    response = client.get("/anime")
+    assert response.status_code == 200
+
